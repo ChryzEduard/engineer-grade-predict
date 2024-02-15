e277b3a214745f1c5ec06ecb7439601ed382331b
Chryz Eduard Q. Mamugos <95526590+ChryzEduard@users.noreply.github.com>
2024-02-16 01:51:01 +0800

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+
+def load_eng_model():
+    with open("rf_eng_model.pkl", "rb") as file:
+        eng_model = pickle.load(file)
+    return eng_model
