567943509ad1b949e1f105814a4238bb31bfb97d
Chryz Eduard Q. Mamugos <95526590+ChryzEduard@users.noreply.github.com>
2024-02-17 20:27:58 +0800

    Delete predict.py

--- predict.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pickle
-import numpy as np
-
-
-def load_eng_model():
-    with open("rf_student_model.pkl", "rb") as file:
-        eng_model = pickle.load(file)
-    return eng_model
